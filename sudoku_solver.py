979da26560ae4b3b65219db6ffc2e5fdb0258d91
Andy Wong Dai <andihuang329@hotmail.com>
2020-08-25 00:11:15 +1200

    Added validation checks for sudoku constraints

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,3 +1,30 @@
+def check_row(board, num , position):
+    for i in range(len(board)):
+        if board[position[0]][i] == num and position[1] != i:
+            return False
+    
+
+def check_column(board, num , position):
+    for i in range(len(board)):
+        if board[i][position[1]] == num and position[0] != i:
+            return False
+    
+
+def check_subgrid(board, num , position):
+    box_x = position[1] // 3
+    box_y = position[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == num and (i,j) != position:
+                return False
+
+def is_valid(board, num , position):
+    check_row(board, num , position)
+    check_column(board, num , position)
+    check_subgrid(board, num , position)
+    return True
+
 def find_empty_space(board):
     for i in range(len(board)):
         for j in range(len(board)):
@@ -13,7 +40,7 @@ def print_board(board):
 
         for j in range(len(board[0])):
             if j % 3 == 0 and j != 0:
-                print(" | ", end="")
+                print(" | ", end=  "")
 
             if j == 8:
                 print(board[i][j])
